811978a3c1cc5a15a832703232e5786c38d6c571
Crowdbotics <team@crowdbotics.com>
2020-12-24 05:48:38 +0000

    Update models

--- backend/vitawerks_23533/wsgi.py
+++ backend/vitawerks_23533/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vitawerks_23533.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vitawerks_23533.settings")
 
 application = get_wsgi_application()
